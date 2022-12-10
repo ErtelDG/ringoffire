5650196080f94dd613dfef112fb9d316a94e26c5
ErtelDG <ertel.developer@t-online.de>
2022-12-10 09:28:29 +0100

    ringOfFire

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.html"],
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        8: "repeat(8, minmax(0, 1fr))",
+        10: "repeat(10, minmax(0, 1fr))",
+        12: "repeat(12, minmax(0, 1fr))",
+        14: "repeat(14, minmax(0, 1fr))",
+      },
+      gridRowEnd: {
+        8: "8",
+        9: "9",
+        10: "10",
+        11: "11",
+        12: "12",
+        13: "13",
+      },
+      gridRowStart: {
+        8: "8",
+        9: "9",
+        10: "10",
+        11: "11",
+        12: "12",
+        13: "13",
+      },
+    },
+  },
+  plugins: [],
+};

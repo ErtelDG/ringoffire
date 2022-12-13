0076c6c32187dc4f3914fad7a93b430143a1276d
ErtelDG <ertel.developer@t-online.de>
2022-12-13 12:24:55 +0100

    save

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -12,9 +12,10 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./game.component.scss'],
 })
 export class GameComponent {
-  // globale variable für diese component
-  pickCardAnimation = false;
-  currentCard: any = '';
+  // globale variable für diese component um diese globale für alle zu synchronisieren über firebase, müssen diese variable nachj game.ts wo auch die spieler-, kartendaten usw. hinterlegt sind => jetzt auskommentiert sind in game.ts
+  //pickCardAnimation = false;
+  //currentCard: any = '';
+
   game: Game = new Game();
   gameId: string = '';
 
@@ -32,6 +33,7 @@ export class GameComponent {
 
       // datenbank automatisch werte bei änderungen abrufen: =>valueChanges().subscribe((game) => {console.log('Game update', game); });
       //mit doc() wird auf die satei zugegriffen bzw. verzeichnis von der collection
+      //subscribe ruft die nachfolgende genannte daten ab um diese zu synchonisieren, sobald diese geändert werden auch pickCardAnimation und  currentCard müssen hinzugefügt werden;
       this.firestore
         .collection('games')
         .doc(this.gameId)
@@ -42,6 +44,8 @@ export class GameComponent {
           this.game.playerCard = game['playerCard'];
           this.game.players = game['players'];
           this.game.stack = game['stack'];
+          this.game.pickCardAnimation = game['pickCardAnimation'];
+          this.game.currentCard = game['currentCard'];
         });
     });
   }
@@ -56,18 +60,18 @@ export class GameComponent {
   }
 
   takeCard() {
-    if (!this.pickCardAnimation) {
-      this.currentCard = this.game.stack.pop();
-      this.pickCardAnimation = true;
-      this.saveGame();
-
+    if (!this.game.pickCardAnimation) {
+      this.game.currentCard = this.game.stack.pop();
+      this.game.pickCardAnimation = true;
       this.game.currentPlayer++;
       this.game.currentPlayer =
         this.game.currentPlayer % this.game.players.length;
+      //obere änderungen werden sofort wieder gesendet und gespeicher in firebase
+      this.saveGame();
 
       setTimeout(() => {
-        this.game.playerCard.push(this.currentCard);
-        this.pickCardAnimation = false;
+        this.game.playerCard.push(this.game.currentCard);
+        this.game.pickCardAnimation = false;
         this.saveGame();
       }, 1000);
     }
--- src/models/game.ts
+++ src/models/game.ts
@@ -4,6 +4,10 @@ export class Game {
   public playerCard: any[] = [];
   public currentPlayer: number = 0;
 
+  //variable hier neu um diese global zu synchroniusieren und von alle genutzt werden können z.B. css wegen style binding
+  public pickCardAnimation = false;
+  public currentCard: any = '';
+
   constructor() {
     for (let i = 1; i < 14; i++) {
       this.stack.push('ace_' + i);
@@ -17,11 +21,14 @@ export class Game {
   // public damit überall zugegriffen werden kann
   public toJson() {
     // object in ein json umwandeln, damt es an firebase / backend gesendet werden kann
+    // public pickCardAnimation = false; public currentCard: any = ''; ebenfalls hinzufügen, damit diese synchronisiert werden
     return {
       players: this.players,
       stack: this.stack,
       playerCard: this.playerCard,
       currentPlayer: this.currentPlayer,
+      pickCardAnimation: this.pickCardAnimation,
+      currentCard: this.currentCard,
     };
   }
 }
